5e13187fdd6be81ce658528f932129262312db78
Khanos <epilef.rodriguez@gmail.com>
2015-08-07 10:23:08 -0430

    Este es el primer commit para las pruebas con MEAN Stack

---
+++ server/models/Movie.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+// Create the MovieSchema.
+var MovieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+//    index: true,
+    unique: true,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  description: {
+  	type: String,
+  	required: true
+  }
+});
+MovieSchema.plugin(uniqueValidator, { message: 'Error, se espera que {PATH} sea unico.' });
+
+// Export the model schema.
+module.exports = MovieSchema;
